12c98f96b493d72081bf07c4e25c35455f696e6c
sp4c3j1mm1 <51430179+sp4c3j1mm1@users.noreply.github.com>
2019-06-17 22:28:55 -0300

    Create design_controle.vhd

---
+++ design_controle.vhd
@@ -0,0 +1,19 @@
+--Ramon Ferreira 
+--junho/2019
+
+-- Bloco de controle.
+
+entity controle is
+port(
+btLD, btS, btVEL, BtF: in bit;
+s1, s2, s3, s4: out bit);
+end entity controle;
+
+architecture comportamento of controle is
+begin
+s1 <= (btLD and btS and (not BtF));
+s2 <= (btLD and (not btS) and (not BtF));
+s3 <= (((not btS)and(BtLD))or(BtLD and BtF));
+s4 <= (((btS)and(BtLD))or(BtLD and BtF));
+
+end architecture;
