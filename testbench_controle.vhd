c70ffe5ea889f6d8f18d22717a9fea13e9a001be
sp4c3j1mm1 <51430179+sp4c3j1mm1@users.noreply.github.com>
2019-06-18 13:55:52 -0300

    Update testbench_controle.vhd

--- testbench_controle.vhd
+++ testbench_controle.vhd
@@ -17,63 +17,51 @@ signal btld_in, bts_in, btvel_in, btf_in, s1_out, s2_out, s3_out, s4_out : bit;
 
 begin
 
-PORRA: controle port map(btld_in,bts_in,btvel_in,btf_in,s1_out,s2_out,s3_out,s4_out: bit);
+DUT: controle port map(btld_in,bts_in,btvel_in,btf_in,s1_out,s2_out,s3_out,s4_out);
 
 process 
 begin
 
-btLD_in <= "0";
-btS_in <= "0";
-btVEL_in <= "0";
-BtF_in <= "0";
+btLD_in <= '0';
+btS_in <= '0';
+btVEL_in <= '0';
+BtF_in <= '0';
 wait for 1 ns;
 
-btLD_in <= "1";
-btS_in <= "0";
-btVEL_in <= "0";
-BtF_in <= "0";
+btLD_in <= '1';
+btS_in <= '1';
+btVEL_in <= '0';
+BtF_in <= '0';
 wait for 1 ns;
 
-btLD_in <= "1";
-btS_in <= "0";
-btVEL_in <= "0";
-BtF_in <= "1";
+btLD_in <= '1';
+btS_in <= '1';
+btVEL_in <= '0';
+BtF_in <= '0';
 wait for 1 ns;
 
-btLD_in <= "1";
-btS_in <= "0";
-btVEL_in <= "1";
-BtF_in <= "0";
+btLD_in <= '1';
+btS_in <= '1';
+btVEL_in <= '0';
+BtF_in <= '0';
 wait for 1 ns;
 
-btLD_in <= "1";
-btS_in <= "0";
-btVEL_in <= "1";
-BtF_in <= "1";
+btLD_in <= '1';
+btS_in <= '0';
+btVEL_in <= '1';
+BtF_in <= '0';
 wait for 1 ns;
 
-btLD_in <= "1";
-btS_in <= "1";
-btVEL_in <= "0";
-BtF_in <= "0";
+btLD_in <= '1';
+btS_in <= '0';
+btVEL_in <= '1';
+BtF_in <= '0';
 wait for 1 ns;
 
-btLD_in <= "1";
-btS_in <= "1";
-btVEL_in <= "0";
-BtF_in <= "1";
-wait for 1 ns;
-
-btLD_in <= "1";
-btS_in <= "1";
-btVEL_in <= "1";
-BtF_in <= "0";
-wait for 1 ns;
-
-btLD_in <= "1";
-btS_in <= "1";
-btVEL_in <= "1";
-BtF_in <= "1";
+btLD_in <= '1';
+btS_in <= '0';
+btVEL_in <= '1';
+BtF_in <= '1';
 wait for 1 ns;
 
 wait;
