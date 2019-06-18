a471880aaad4f0c991f5346b51eadbb389ef91d8
sp4c3j1mm1 <51430179+sp4c3j1mm1@users.noreply.github.com>
2019-06-18 02:03:41 -0300

    Update testbench_ponte_H.vhd

--- testbench_ponte_H.vhd
+++ testbench_ponte_H.vhd
@@ -14,5 +14,28 @@ port (J0, J1, J2, K0, K1, K2, clk: in bit;
 Q0, Q0i, Q1, Q1i, Q2, Q2i, jj2, jj0, kk1: out bit);
 end component FFJK_ponteH;
 
-signal clk, stop, 
+--Sinais de entrada dos FFs.
+signal clk, stop, q0, q0i, q1, q1i, q2, q2i : bit;
+
+begin
+--Separação de cada FF.
+FFJK0: FFJK_ponteH port map(J0 <= (Q1i or Q2i), K0 <= '1', clk <= clk, Q0 <= q0, Q0i <= q0i);
+FFJK1: FFJK_ponteH port map(J1 <= Q0, K1 <= (Q0 or Q2),clk <= clk, Q1 <= q1, Q1i <= q1i);
+FFJK2: FFJK_ponteH port map(J2 <= (Q0 and Q1), K2 <= Q1, clk <= clk, Q2 <= q2, Q2i < q2i);
+
+--Clock
+process begin
+while stop = '0' loop
+clk <= not clk;
+wait for 1ns;
+end loop;
+wait;
+end process;
+
+process begin
+wait for 36ns;
+stop <= '1';
+wait;
+end process;
+
 end architecture comportamento;
